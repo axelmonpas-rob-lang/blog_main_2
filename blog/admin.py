46a2d03ad91a7a07028f67d2f3ca13fe5031b4e9
monroy <axelmonpas@gmail.com>
2026-03-17 17:37:58 -0700

    admin confi

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import post, category, Comment
 # Register your models here.
+
+
+admin.site.register(post)
+admin.site.register(category)
+admin.site.register(Comment)

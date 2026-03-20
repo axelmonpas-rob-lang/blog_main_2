1425b031c3f964d3e74e2877bbd135a142dae1a2
axelmonpas-rob-lang <axelmonpas@gmail.com>
2026-03-19 22:17:52 -0700

    tablas

--- blog/models.py
+++ blog/models.py
@@ -1,29 +1,35 @@
-from django.db import models 
+from django.db import models
+
 
 # Create your models here.
 class category (models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField()
 
+
     class Meta:
-        ordering = ('title')
+        ordering = ('title',)
         verbose_name_plural = 'Categories'
+
 
     def __str__(self):
         return self.title
-    
+   
     def get_absolute_url(self):
          return '/%s/' % self.slug  
-    
-class post(models.Mode):
+   
+class post(models.Model):
+
 
         ACTIVE= 'active'
         DRAFT= 'draft'
+
 
         CHOICES_STATUS = {
             (ACTIVE, 'active'),
             (DRAFT, 'draft')
         }
+
 
         category = models.ForeignKey(category, related_name='post', on_delete=models.CASCADE)
         title = models.CharField(max_length=255)
@@ -33,15 +39,19 @@ class post(models.Mode):
         status = models.CharField(max_length=10, choices=CHOICES_STATUS, default=ACTIVE)
         image = models.ImageField(upload_to='upload/', blank=True, null=True)
 
+
         def __str__(self):
             return self.title
        
-class Comment (models.Mode):
+class Comment (models.Model):
     post = models.ForeignKey(post, related_name='comments', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     email = models.EmailField()
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
+
     def __str__(self):
         return self.name
+
+

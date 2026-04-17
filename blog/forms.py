886ef3d8ab536e611318d89b241896a964ca56c8
axelmonpas-rob-lang <axelmonpas@gmail.com>
2026-04-16 17:33:40 -0700

    “nojalo”

--- blog/forms.py
+++ blog/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 from .models import Comment
 
-class CommentFrom(forms.modelform):
+class CommentForm(forms.modelform):
     class Meta:
         model = Comment
         fields = ['name','email','body']
